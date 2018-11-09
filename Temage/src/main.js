019064d7fcd2ad72b5f36ba946a21e86a7dd217a
Miaow~ <wxmxl@hotmail.com>
2018-11-09 19:44:08 +0800

    iview installed.

--- Temage/src/main.js
+++ Temage/src/main.js
@@ -6,10 +6,13 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/index.js'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
