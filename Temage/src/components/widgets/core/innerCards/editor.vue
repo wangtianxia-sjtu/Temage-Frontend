065a9911ecc382b5c18b4dfdf28b6f7eeb7de038
WangTianxia <sometimesnaive@sjtu.edu.cn>
2018-11-13 21:45:49 +0800

    add core editor

---
+++ Temage/src/components/widgets/core/innerCards/editor.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+      <div ref="editor" style="text-align:left"></div>
+      <br>
+      <b-button v-on:click="getContent" style="margin: 0px auto;display: table;">需要HTML代码?</b-button>
+    </div>
+</template>
+
+<script>
+import E from 'wangeditor'
+export default {
+  name: 'editor',
+  data () {
+    return {
+      editorHTML: ''
+    }
+  },
+  methods: {
+    getContent: function () {
+      alert(this.editorHTML)
+    }
+  },
+  mounted () {
+    var editor = new E(this.$refs.editor)
+    editor.customConfig.onchange = (html) => {
+      this.editorHTML = html
+    }
+    this.editorHTML = this.editorContent
+    editor.create()
+    editor.txt.html(this.editorHTML)
+    editor.change()
+  },
+  props: [ 'editorContent' ]
+  /* name: 'editor',
+  methods: {
+    getContent: function () {
+      alert(this.editorContent)
+    }
+  },
+  mounted () {
+    var editor = new E(this.$refs.editor)
+    editor.customConfig.onchange = (html) => {
+      this.editorContent = html
+    }
+    editor.create()
+    editor.txt.html(this.editorContent)
+    editor.change()
+  },
+  props: [ 'editorContent' ] */
+}
+</script>
+
+<style scoped>
+</style>
